37c293b74c4f4db56a59b41e56f003c74a4b2bde
madsab <mads.barnes@hotmail.com>
2026-06-18 10:29:10 +0200

    Spinning kasper

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,13 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home" />
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
+      <img className={styles.img} src="https://i0.wp.com/computas.com/wp-content/uploads/2025/01/IMG_5656.jpg?resize=2048%2C1536&ssl=1" alt="Home"/>
     </div>
   );
 }
